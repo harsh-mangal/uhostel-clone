75ef6067b8ee2112de93306d637ed880edf2092b
Dhruv Sharma <dhruv0198.be20@chitkara.edu.in>
2023-08-23 22:25:08 +0530

    1

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 import { Link } from 'react-router-dom'
 
 const Password = () => {
+  const [formData , setFormData]=useState({
+    name:"",
+    roomNumber:"",
+    rollNumber:"",
+    newPassword:""
+  });
+
+  const handleChange=(e)=>{
+      const {name, value}=e.target;
+      setFormData((prevData)=>({
+        ...prevData,
+        [name]:value
+      }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+         alert("DONE");
+         
+      } else {
+        alert("NO");
+      }
+    } catch (error) {
+      console.error('An error occurred', error);
+    }
+  };
+
   return (
     <div>
         <div className="navbar">
@@ -40,23 +78,25 @@ const Password = () => {
           />
         </div>
         <div>
-          <h4>Hi,HARSH MANGAL</h4>
+          <h4>Hi</h4>
           <h3>ADMIN</h3>
           <h3>WARDEN SECTION</h3>
         </div>
       </div>
         <div className='passpanel'>
+            <form onSubmit={handleSubmit}>
             <h4>Name</h4>
-            <input type="name" placeholder='Enter your Name'/>
+            <input type="name" name='name' value={formData.name} onChange={handleChange} placeholder='Enter your Name'/>
             <br />
             <h4>Roll Number</h4>
-            <input type="number" placeholder='Enter your Roll Number'/>
+            <input type="number" name='rollNumber' value={formData.rollNumber} onChange={handleChange} placeholder='Enter your Roll Number'/>
             <br />
             <h4>Room Number</h4>
-            <input type="number" placeholder='Enter your Room Number'/>
+            <input type="number" name='roomNumber' value={formData.roomNumber} onChange={handleChange} placeholder='Enter your Room Number'/>
             <h4>Password</h4>
-            <input type="password" placeholder='Confirm new password'/>
+            <input type="password" name='newPassword' value={formData.newPassword} onChange={handleChange} placeholder='Confirm new password'/>
             <button className='sbp'>Submit</button>
+            </form>
         </div>
        
     </div>
