b0ba8e663032f751b2e1b2502434536f0ecbf36d
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2023-08-23 19:07:17 +0530

    tp

--- server.js
+++ server.js
@@ -6,3 +6,4 @@ const port = 5000;
 mongoose.connect('mongodb+srv://devharshmangal:harsh786@cluster0.pwltp9k.mongodb.net/UHostel?retryWrites=true&w=majority',
 console.log('mongodb is running'));
 app.listen(port , console.log('server listening on port ' + port));
+const jh = hello;
